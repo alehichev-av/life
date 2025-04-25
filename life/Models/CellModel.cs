3455ffaf805dcbf1d2e1ac789d7499e6e5b7511f
torvalds <torvalds@GNULAPTOP>
2025-04-26 00:06:50 +0300

    Changed to dotnet 6.0

---
+++ life/Models/CellModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace life.Models
+{
+    internal class CellModel
+    {
+        public bool IsAlive {
+            get;
+            set;
+        }
+        public CellModel(bool alive = false)
+        {
+            IsAlive = alive;
+        }
+        public void Toggle()
+        {
+            IsAlive = !IsAlive;
+        }
+    }
+}
