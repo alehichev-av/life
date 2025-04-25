9c51693801cc0555a01756b64bce82c317fec105
torvalds <torvalds@GNULAPTOP>
2025-04-26 00:19:34 +0300

    Added MVVM Class library

---
+++ MVVM/BindableBase.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM;
+
+public class BindableBase : INotifyPropertyChanged
+{
+    /// <summary>
+    ///     Multicast event for property change notifications.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    ///     Checks if a property already matches a desired value.  Sets the property and
+    ///     notifies listeners only when necessary.
+    /// </summary>
+    /// <typeparam name="T">Type of the property.</typeparam>
+    /// <param name="storage">Reference to a property with both getter and setter.</param>
+    /// <param name="value">Desired value for the property.</param>
+    /// <param name="propertyName">
+    ///     Name of the property used to notify listeners.  This
+    ///     value is optional and can be provided automatically when invoked from compilers that
+    ///     support CallerMemberName.
+    /// </param>
+    /// <returns>
+    ///     True if the value was changed, false if the existing value matched the
+    ///     desired value.
+    /// </returns>
+    public bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(storage, value)) return false;
+        storage = value;
+        RaisePropertyChanged(propertyName);
+        return true;
+    }
+
+    protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
---
+++ MVVM/Extensions/CommandExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+using MVVM.Commands;
+
+namespace MVVM.Extensions;
+
+public static class CommandExtensions
+{
+    /// <summary>
+    /// Triggers the CanExecuteChanged event and a property changed event on the CanExecute property.
+    /// </summary>
+    /// <param name="command">Must be <see cref="RelayCommand"/></param>
+    public static void RaiseCanExecuteChanged(this ICommand command) => ((RelayCommand)command).RaiseCanExecuteChanged();
+}
